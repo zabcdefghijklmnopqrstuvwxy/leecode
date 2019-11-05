caa703f9c8ea9223d4f577b9994162212ddad9c9
sai <sai14790085178@163.com>
2019-11-05 23:05:33 +0800

    修复累加和超出int类型的Bug,改用递归求和，leecode测试通过

--- LeeCode/topic2/main.c
+++ LeeCode/topic2/main.c
@@ -5,104 +5,21 @@ struct ListNode {
       struct ListNode *next;
 };
 
+int c = 0;
+
+/**
+@brief 递归累加
+*/
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
 {
-    int nSum = 0,nL1 = 0,nL2 = 0;
-    int nNodeCnt = 0;
-    struct ListNode *result = NULL;    
-    struct _STACK
-    {
-        int val;
-        struct _STACK *pre;
-    };
-    
-    struct ListNode *tmpl1 = l1;
-    struct ListNode *tmpl2 = l2;
-    struct _STACK *tmps1 = NULL;
-    struct _STACK *tmps2 = NULL;
-
-
-    if(NULL == l1 || NULL == l2)
-    {
-        return NULL;
-    }
-
-    while(NULL != tmpl1)
-    {
-        static nL1Cnt = 0;
-        struct _STACK *sl1 = (struct _STACK*)malloc(sizeof(struct _STACK));
-        memset(sl1,0,sizeof(struct _STACK));
-        sl1->val = tmpl1->val;
-        if(nL1Cnt)
-        {
-            sl1->pre = tmps1;
-        }
-        tmpl1 = tmpl1->next;
-        tmps1 = sl1;
-        nL1Cnt++;
-    }
-
-    while(NULL != tmpl2)
-    {
-        static nL2Cnt = 0;
-        struct _STACK *sl2 = (struct _STACK*)malloc(sizeof(struct _STACK));
-        memset(sl2,0,sizeof(struct _STACK));
-        sl2->val = tmpl2->val;
-        if(nL2Cnt)
-        {
-            sl2->pre = tmps2;
-        }
-        tmpl2 = tmpl2->next;
-        tmps2 = sl2;
-        nL2Cnt++;
-    }
-
-    while(NULL != tmps1)
-    {
-        struct _STACK *tmp = NULL;
-        nL1 = nL1*10 + tmps1->val;
-        tmp = tmps1;
-        tmps1 = tmps1->pre;
-        free(tmp);
-    }
-    
-    while(NULL != tmps2)
-    {
-        struct _STACK *tmp = NULL;
-        nL2 = nL2*10 + tmps2->val;
-        tmp = tmps2;
-        tmps2 = tmps2->pre;
-        free(tmp);
-    }
-
-    nSum = nL1 + nL2;
-
-    while(nSum)
-    {
-        static struct ListNode *tmp = NULL;
-        int val = 0;
-        val = nSum % 10;
-        struct ListNode *newnode = (struct ListNode*)malloc(sizeof(struct ListNode));
-        memset(newnode,0,sizeof(struct ListNode));
-
-        newnode->val = val;
-        newnode->next = NULL;
-
-        if(nNodeCnt)
-        {
-            tmp->next = newnode;
-        }
-        else
-        {
-            result = newnode;
-        }
-        
-        tmp = newnode;
-        nSum = nSum / 10;
-        nNodeCnt++;
-    }
-
-    return result;
+	if(l1==NULL&&l2==NULL&&c==0)return NULL;
+    l1!=NULL?(c+=l1->val,l1=l1->next):(c+=0);
+    l2!=NULL?(c+=l2->val,l2=l2->next):(c+=0);
+    struct ListNode *cur=(struct ListNode *)malloc(sizeof(struct ListNode));
+    cur->val=c%10;
+    c=c/10;
+    cur->next=addTwoNumbers(l1,l2);
+    return cur;
 }
 
 
