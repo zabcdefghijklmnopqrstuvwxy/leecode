6599ca3ab3e476699d45460dc428239f54d0597d
sai <sai14790085178@163.com>
2019-12-23 22:42:34 +0800

    2019-12-23 Sai *)优化旋转图像代码，遵循先转置矩阵后镜像的算法

--- LeeCode/topic48/main.c
+++ LeeCode/topic48/main.c
@@ -1,50 +1,29 @@
 #include<stdio.h>
 
+/**
+* @brief 先矩阵转置然后镜像即可
+*/
 void rotate(int** matrix, int matrixSize, int* matrixColSize)
 {
-    int i = 0,j = 0;
-    int nMatrixRowSize = *matrixColSize;
-    int nTemp = 0;
+    int n = matrixSize;
+    int tmp = 0;
+    *matrixColSize = n;
 
-    for(i = 0; i < nMatrixRowSize/2; i++)
-    {
-        for(j = 0; j < nMatrixRowSize; j++)
-        {
-            nTemp = *((int*)matrix + i*nMatrixRowSize + j);
-            *((int*)matrix + i*nMatrixRowSize + j) = *((int*)matrix + j*nMatrixRowSize + i);
-            *((int*)matrix + j*nMatrixRowSize + i) = nTemp;
-        }
+     for (int i = 0; i < n; i++) {
+      for (int j = i; j < n; j++) {
+        int tmp = matrix[j][i];
+        matrix[j][i] = matrix[i][j];
+        matrix[i][j] = tmp;
+      }
+    }
+    // reverse each row
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n / 2; j++) {
+        int tmp = matrix[i][j];
+        matrix[i][j] = matrix[i][n - j - 1];
+        matrix[i][n - j - 1] = tmp;
+      }
     }
 
-    for(i = 0; i < nMatrixRowSize; i++)
-    {
-        for(j = 0; j < nMatrixRowSize / 2; j++)
-        {
-            nTemp = *((int*)matrix + i*nMatrixRowSize + j);
-            *((int*)matrix + i*nMatrixRowSize + j) = *((int*)matrix + i*nMatrixRowSize + nMatrixRowSize - j - 1);
-            *((int*)matrix + i*nMatrixRowSize + nMatrixRowSize - j - 1) = nTemp;
-        }
-    }
-}
-
-int main(void)
-{
-    int matrix[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-    int nColSize = 3;
-    int i = 0,j = 0;
-    int *pmatrix = matrix;
-
-    rotate(matrix, sizeof(matrix), &nColSize);
-    
-    for(i = 0; i < nColSize; i++)
-    {
-        for(j = 0; j < nColSize; j++)
-        {
-            printf("%d\t",matrix[i][j]);
-        }
-        printf("\n");
-    }
-
-    return 0;
 }
 
