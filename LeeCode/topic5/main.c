225dabe03a6fae1b79788bc0eb8c8333887fb3fa
sai <sai14790085178@163.com>
2019-12-24 15:55:56 +0800

    2019-12-24 Sai *)修复topic5中回文字符串不发通过leecode的bug,采用中心扩散法找寻最长的回文字符串

--- LeeCode/topic5/main.c
+++ LeeCode/topic5/main.c
@@ -1,126 +1,38 @@
 #include<stdio.h>
 
-char result[128] = {0};
-
-char * longestPalindrome(char * s)
+int expandaroundcenter(char *s, int left, int right)
 {
-    int Hash[128] = {0};
-    char fix[26] = {0};
-    char *pLongPos = NULL;
-    int nCnt = 0;
-    int nStrLen = 0;
-    int nIndex = 0;
-    int nPos = 0;
-    int i = 0,j = 0;
-    int nFlag = 0;
-    int nLen = strlen(s);
-
-    if(nLen <= 1)
+    int L = left, R = right;
+    while (L >= 0 && R < strlen(s) && s[L] == s[R]) 
     {
-        return s;
+        L--;
+        R++;
     }
-    
-    while(nPos < nLen && i < nLen)
+    return R - L - 1;
+}
+
+/**
+* @brief 中心扩散法寻找回文中心位置，以此位置进行扩散寻找最长的回文串
+*/
+
+char * longestPalindrome(char * s){
+    if (strlen(s) == 0) return "";
+    int start = 0, end = 0;
+    for (int i = 0; i < strlen(s); i++) 
     {
-        if(0 == Hash[s[i]])
+        int len1 = expandaroundcenter(s, i, i);
+        int len2 = expandaroundcenter(s, i, i+1);
+        int len = len1 > len2 ? len1 : len2;
+        if (len > end - start) 
         {
-            Hash[s[i]] = ++nIndex;
-            fix[j++] = nIndex;
-            i++;
+            start = i - ((len - 1)>>1);
+            end = i + (len>>1);
         }
-        else
-        {
-            int nTemPos = Hash[s[i]];
-            
-            if(nTemPos == fix[--j])
-            {
-                if(j == 0)
-                {
-                    if(nIndex > nCnt)
-                    {
-                        nCnt = nIndex;
-                        if(nFlag)
-                        {
-                            nStrLen = 2 * nCnt - 1;
-                        }
-                        else
-                        {
-                            if(1 == nCnt)
-                            {
-                                nStrLen = 1;
-                            }
-                            else
-                            {
-                                nStrLen = 2 * nCnt;
-                            }
-                        }
-
-                        pLongPos = s + nPos;
-
-                        nFlag = 0;
-                        nIndex = 0;
-                        memset(Hash,0,sizeof(Hash));
-                        memset(fix,0,sizeof(fix));
-                        ++nPos;
-                        i = nPos;
-                        j = 0;
-                    }
-                }
-            }
-            else
-            {
-                if(0 == nFlag)
-                {
-                    nFlag = 1;
-                    if(nTemPos == fix[--j])
-                    {
-                        if(j == 0)
-                        {
-                            if(nIndex > nCnt)
-                            {
-                                nCnt = nIndex;
-                                if(nFlag)
-                                {
-                                    nStrLen = 2 * nCnt - 1;
-                                }
-                                else
-                                {
-                                    nStrLen = 2 * nCnt;
-                                }
-
-                                pLongPos = s + nPos;
-
-                                nFlag = 0;
-                                nIndex = 0;
-                                memset(Hash,0,sizeof(Hash));
-                                memset(fix,0,sizeof(fix));
-                                ++nPos;
-                                i = nPos;
-                                j = 0;
-                            }
-                       }
-                    }
-                }
-                else
-                {
-                    nFlag = 0;
-                    nIndex = 0;
-                    memset(Hash,0,sizeof(Hash));
-                    memset(fix,0,sizeof(fix));
-                    ++nPos;
-                    i = nPos;
-                    j = 0;
-                }
-            }
-        
-        }
-    
     }
-    
-    memcpy(result,pLongPos,nStrLen);
-    result[nStrLen] = '\0';
-
-    return result;
+    char *p = malloc(sizeof(char) * (end - start + 2));
+    strncpy(p, s + start, end - start + 1);
+    p[end - start + 1] = '\0';
+    return p;
 }
 
 int main(void)
