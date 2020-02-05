94505440d724795d06b1ad4ffc7534ac128efd03
sai <sai14790085178@163.com>
2020-02-05 09:58:53 +0800

    2020-02-05 sai *)topic55题目解题代码补全

--- LeeCode/topic55/main.c
+++ LeeCode/topic55/main.c
@@ -2,5 +2,28 @@
 
 
 bool canJump(int* nums, int numsSize){
+	 int nFast = numsSize - 1;
+    int nSlow = numsSize - 1;
+    bool nRet = false;
 
+    if(numsSize <= 1)
+    {
+        return true;
+    }
+
+    while(nFast > 0)
+    {
+        nFast--;
+        if(nums[nFast] >= (nSlow - nFast))
+        {
+            nSlow = nFast;
+            nRet = true;
+        }
+        else
+        {
+            nRet = false;
+        }
+    }
+
+    return nRet;
 }
