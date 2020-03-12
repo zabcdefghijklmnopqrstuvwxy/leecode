3ba258f1877ee409260cdec8308308546e3b0c1f
sai <sai14790085178@163.com>
2020-03-12 11:53:10 +0800

    2020-03-12 sai *)增加topic739《每日温度》，修复之前leecode测试超时的问题，改进算法采用栈单调减处理可以优化时间，leecode测试通过

--- LeeCode/topic739/main.c
+++ LeeCode/topic739/main.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 
+#if 0
 /**
- * Note: The returned array must be malloced, assume caller calls free().
+ * @brief 暴力解法，leecode测试超时
  */
 int* dailyTemperatures(int* T, int TSize, int* returnSize){
     int *ret = (int*)malloc(sizeof(int) * 30000);
@@ -17,4 +18,56 @@ int* dailyTemperatures(int* T, int TSize, int* returnSize){
         ret[i] = (find - i) > 0 ? (find-i) : 0;
     }
     return ret;
+}
+#endif
+
+#define STACK_LEN 30002
+
+typedef struct tagStackNode{
+    int temper;
+    int index;
+}StackNode;
+
+void StackPopProc(StackNode *stack, int *top, int index, int T, int *retArray)
+{
+    while (*top >= 0 && stack[*top].temper < T) {
+        retArray[stack[*top].index] = index - stack[*top].index;
+        (*top)--;
+    }
+
+    stack[++(*top)].temper = T;
+    stack[(*top)].index = index;
+}
+
+/**
+ * @brief 栈单调减处理
+ */
+int* dailyTemperatures(int* T, int TSize, int* returnSize){
+    int *retArray;
+    StackNode stack[STACK_LEN];
+    int top = -1;
+    int i = 0;
+
+    if (TSize == 0) {
+        *returnSize = 0;
+        return NULL;
+    }
+
+    retArray = (int *)malloc(sizeof(int) * TSize);
+    memset(retArray, 0, sizeof(int) * TSize);
+
+    stack[++top].temper = T[i];
+    stack[top].index = i;
+
+    for (i = 1; i < TSize; i++) {
+        if (T[i] <= stack[top].temper) {
+            stack[++top].temper = T[i];
+            stack[top].index = i;
+        }else {
+            StackPopProc(stack, &top, i, T[i], retArray);
+        }
+    }
+
+    *returnSize = TSize;
+    return retArray;
 }
