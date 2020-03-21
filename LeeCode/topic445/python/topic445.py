da61fed800634105b86f85b7a9315f968673d5e2
sai <sai14790085178@163.com>
2020-03-21 15:57:51 +0800

    2020-03-21 sai +)增加topic445《两数相加II》，python测试通过 @)人生苦短，快用python。C真是不好用

---
+++ LeeCode/topic445/python/topic445.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        num1 = ''
+        num2 = ''
+        while l1:
+            num1 += str(l1.val)
+            l1 = l1.next
+        while l2:
+            num2 += str(l2.val)
+            l2 = l2.next
+        add = str(int(num1) + int(num2))
+        head = ListNode(add[0])
+        answer = head
+        for i in range(1, len(add)):
+            node = ListNode(add[i])
+            head.next = node
+            head = head.next
+        return answer
