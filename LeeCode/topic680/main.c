71d31030bfe58cdc1cd6e6b0bb200f0398d99cf1
sai <sai14790085178@163.com>
2020-05-10 16:35:37 +0800

    2020-05-10 sai +)增加topic680《验证回文字符串II》，leecode测试通过

---
+++ LeeCode/topic680/main.c
@@ -0,0 +1,23 @@
+bool isPalindrome(char* s,int l,int r){
+    while(l < r)
+    {
+        if(s[l] != s[r])
+            return false;
+        l++;
+        r--;
+    }
+    return true;
+}
+bool validPalindrome(char* s) {
+    int l = 0,r = strlen(s) - 1;
+    while(l < r)
+    {
+        if(s[l] != s[r])
+        {
+            return isPalindrome(s,l+1,r) || isPalindrome(s,l,r-1);
+        }
+        l++;
+        r--;
+    }
+    return true;
+}
