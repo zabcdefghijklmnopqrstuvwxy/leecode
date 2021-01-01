930a29b7f8078be63a4d9fd2e7c706ad779ddfd2
sai <sai14790085178@163.com>
2021-01-01 20:37:03 +0800

    2021-01-01 sai +)增加剑指Offer64《求 1+2+...+n》，C语言编写，leecode测试通过

--- LeeCode/topic35/main.c
+++ LeeCode/topic35/main.c
@@ -1,6 +1,35 @@
-#include<stdio.h>
-
-
 int searchInsert(int* nums, int numsSize, int target){
+    int nLeft = 0;
+    int nRight = numsSize - 1;
+    int nMid = 0;
+    int nIndex = 0;    
 
+    if(target <= nums[0])
+    {
+        return 0;
+    }
+    
+    if(target > nums[nRight])
+    {
+        return nRight + 1;
+    }
+
+    while(nLeft < (nRight - 1))
+    {
+        nMid = (nLeft + nRight) / 2;
+        if(target == nums[nMid])
+        {
+            return nMid;
+        }
+        else if(target < nums[nMid])
+        {
+            nRight = nMid;
+        }
+        else
+        {
+            nLeft = nMid;
+        }
+    }
+
+    return nLeft + 1; 
 }
